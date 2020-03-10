683ec2373141f0c681ce0daa76b30a8e2979de96
akshadaraskar <52907053+akshadaraskar@users.noreply.github.com>
2020-03-11 00:49:29 +0530

    Entity Name  Annotations
    Changed Table Name & Column Name using Annotations

--- FirstHibernateDemo/src/com/hibernate/dao/UserDetails.java
+++ FirstHibernateDemo/src/com/hibernate/dao/UserDetails.java
@@ -5,11 +5,13 @@ import javax.persistence.Id;
 
 
 
-@Entity
+@Entity(name="USER_DETAILS")
 public class UserDetails 
 {
 	@Id
+	@Column(name="USER_ID")
 	private int userId;
+	@Column(name="USER_NAME")
 	private String userName;
 	
 	public int getUserId() {
